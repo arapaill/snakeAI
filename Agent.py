3b0c3eefd6efe8ba2494be6535fd4864549a94c4
vboxuser <vboxuser@snakeAI.myguest.virtualbox.org>
2023-02-08 11:10:10 +0100

    agent ready for AI, starting AI model

--- Agent.py
+++ Agent.py
@@ -15,6 +15,8 @@ class Agent:
         self.gamma = 0      #discount rate
         self.memory = deque(maxlen=MAX_MEMORY) #erase from the begining when too much memory
         #TODO: model, trainer
+        self.model = None
+        self.trainer = None
 
     def get_state(self, game):
         head = game.snake[0]
@@ -63,14 +65,35 @@ class Agent:
         return np.array(state, dtype=int)
         
     def remember(self, state, action, reward, next_state, game_over):
-        pass
+        self.memory.append((state, action, reward, next_state, game_over)) #popleft when MAX_MEMORY # ONE Tuple
     def train_long_memory(self):
         #replay all the moves done on the short memory and try different move each step
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory)
+        else:
+            mini_sample = self.memory
+        states, actions, rewards, next_states, games_over = zip(*mini_sample) 
+        #zip assemble every element of all the memory by their order:
+        #[(state1, action1, reward1, next_state1, game_over1), (state2, action2, reward2, next_state2, game_over2), ...]
+        self.trainer.train_step(states, actions, rewards, next_states, games_over)
     def train_short_memory(self, state, action, reward, next_state, game_over):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, game_over)
     def get_action(self, state):
-        pass
+        #trade of between exploration / explotation
+        # use some randomness in the first moves and gradually remove it
+        self.epsilon = 80 - self.nb_game #epsilon is the indicator of randomness, here 80 games is hardcoded
+        final_move = [0,0,0] #one of the 0 has to be true
+        if random.randint(0, 200) < self.epsilon: #there is a small chance that a random move is selected, as the epsilon gets smaller, the chance get smaller too
+            move = random.randint(0,2)
+            final_move[move] = 1 #this means that one of the 0's in final_move shall become a 1. it is define by move = random.randint(0,2)
+        else: #in this case, we are not using a random move but a move based on the AI "reflexion"
+            state0= torch.tensor(state, dtype = torch.float) #creat a tensor state0 for pytorch to use
+            prediction = self.model.predict(state0) #the AI process the tensor and give a prediction based on experience
+            move = torch.argmax(prediction).item() # move = index of the maximum value (in this case, a 1)
+            final_move[move] = 1
+        return final_move
+
+            
 
 def train():
     plot_score = []
@@ -101,7 +124,7 @@ def train():
             agent.train_long_memory()
             if score > best_score:
                 best_score = score
-                #agent.model.save()
+                agent.model.save()
             print('Game n°', agent.nb_game, 'Score: ', score, 'Best score: ', best_score)
             #plot
 
